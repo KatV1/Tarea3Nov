b9cdeb50f75b4f352b98dd103390fc2c5c19c2e6
KatV1 <kat.valcarcel@live.com>
2020-11-03 18:54:00 -0500

    MVC, Controlador y Clase

---
+++ Tarea/Models/Producto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Tarea.Models
+{
+    public class Producto
+    {
+        [Required(ErrorMessage = "Por favor, ingrese nombre")]
+        [Display(Name="Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Por favor, ingrese un precio")]
+        [Display(Name="Precio")]
+        //no sé, podría ser decimal o float
+        public double Precio { get; set; }
+
+        [Display(Name="Descripción")]
+        public string Descripcion { get; set; }
+
+    }
+}
